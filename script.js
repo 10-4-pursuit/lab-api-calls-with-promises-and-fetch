d175b7bee9a3bf4abe973f900012a968320696a5
ada lovelace <williambookman@pursuit.org>
2023-11-15 20:36:40 -0500

    added script to html

---
+++ script.js
@@ -0,0 +1,4 @@
+//https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple
+
+const generateQuestionBtn = document.querySelector("button")
+// generateQuestionBtn.addEventListener("click", ())
