6e548553ab62e63f5e258c947be22dc8fa412fb3
ada lovelace <williambookman@pursuit.org>
2023-11-22 16:07:51 -0500

    got answers to show up

--- script.js
+++ script.js
@@ -1,46 +1,98 @@
-//variable selects the new questions button
-const generateQuestionBtn = document.querySelector("#new-questions-btn");
+const questionsURL =
+  "https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple"; //api request
+const generateQuestionBtn = document.querySelector("#new-questions-btn"); //variable selects the new questions button
+const questionCategory = document.querySelector("#category"); //variable selects the category h2 title
+let questionText = document.querySelector("#question"); //variable selects the question area
+const answersElement = document.querySelector("button"); //variable selects the show answer button
+const resultsElement = document.querySelector(".hidden"); // variable selects the hidden section with the correct answers
 
-//variable selects the category h2 title
-let questionCategory = document.querySelector("#category");
+let currentQuestion = 0; //begins at the first question at index 0
+let score = 0; //keeps tab on the score
 
-//variable selects the category h2 title
-let question = document.querySelector("#question");
-console.log(question);
+fetch(questionsURL)
+  .then((res) => res.json())
+  .then((data) => {
+    const questions = data.results;
 
-// let updateDetails = (category) => {
-//   questionCategory.innerText = category;
-// };
+    displayQuestion(questions[currentQuestion]);
 
-// const generateQuestion() = {
-
-// .then(result => {
-//   updateDetails = console.log(result.results[0].category)
-// });
-// }
-
-generateQuestionBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  // questionCategory.innerText = "Kill Tony!"
-
-  fetch(
-    "https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple"
-  )
-    .then((res) => res.json())
-    .then((result) => {
-      console.log(result);
-      questionCategory.innerText = result.results[0].category;
-      question.innerText = result.results[0].question;
-
-      const questionOptions = document.createElement("div");
-      
-      const choices = ["Option 1", "Option 2", "Option 3"];
-
-
-      result.results[0].type;
+    generateQuestionBtn.addEventListener("click", (event) => {
+      event.preventDefault
+      if (currentQuestion < questions.length - 1) {
+        currentQuestion++;
+        displayQuestion(questions[currentQuestion]);
+      } else {
+        resultsElement.textContent = `You answered ${score} out of 10 questions correctly!`;
+      }
     });
-});
+  });
+
+function displayQuestion(question) {
+  questionText.innerText = question.question; //this is the text for the question
+  questionCategory.innerText = question.category
+  const correctAnswer = question.correct_answer;
+  const incorrectAnswers = question.incorrect_answers;
+
+  // questionCategory.innerText = result.results[0].category;
+  //       question.innerText = result.results[0].question;
+
+  question.textContent = questionText;
+
+  answersElement.innerHTML = "";
+
+  const answers = [correctAnswer].concat(incorrectAnswers); //?
+  const shuffledAnswers = answers.sort(() => Math.random() - 0.5); //?
+
+  for (const answer of shuffledAnswers) {
+    const answerButton = document.createElement("button");
+    answerButton.textContent = answer;
+
+    answerButton.addEventListener("click", (event) => {
+      resultsElement.style.display = "block";
+      if (answer === correctAnswer) {
+        resultsElement.textContent = "Correct!";
+      } else {
+        resultsElement.textContent = `Incorrect. The correct answer is ${correctAnswer}`;
+      }
+      // resultsElement.style.display = "none";
+    });
+
+    answersElement.appendChild(answerButton);
+  }
+}
+
+// generateQuestionBtn.addEventListener("click", (event) => {
+//   event.preventDefault();
+
+//       questionCategory.innerText = result.results[0].category;
+//       question.innerText = result.results[0].question;
+
+//       const card = document.querySelector(".card")
+//       const questionOptions = document.createElement("div");
+//       questionOptions.id = ('question-options')
+//       question.insertAdjacentElement('afterend', questionOptions)
+
+//       const label = document.createElement('label');
+//       label.textContent = 'Who are you?';
+//       label.setAttribute('for', 'option1')
+
+//       const optionElement = document.createElement("input");
+//       optionElement.type = "radio";
+//       optionElement.id = "option1";
+//       optionElement.name = "question";
+
+//       label.appendChild(optionElement)
+
+//       // optionElement.value = "option1";
+//       optionElement.label = "option1?"
+//       questionOptions.appendChild(label)
+
+//       // document.body.removeChild(optionElement)
+
+//       // questionOptions.parentNode.removeChild(questionOptions)
+
+//     });
+// });
 
 // console.log(generateQuestion)
 // generateQuestion();
@@ -84,3 +136,14 @@ generateQuestionBtn.addEventListener("click", (event) => {
 //    }
 
 // })
+
+// let updateDetails = (category) => {
+//   questionCategory.innerText = category;
+// };
+
+// const generateQuestion() = {
+
+// .then(result => {
+//   updateDetails = console.log(result.results[0].category)
+// });
+// }
